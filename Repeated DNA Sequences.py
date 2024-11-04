3f393e470f105e84100576bfcfe6097073ed160e
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2024-11-04 09:20:41 -0500

    Update Repeated DNA Sequences.py

--- Repeated DNA Sequences.py
+++ Repeated DNA Sequences.py
@@ -1,3 +1,4 @@
+#my version:
 from collections import Counter
 
 class Solution:
@@ -34,3 +35,22 @@ class Solution:
         # print(res)
 
         return res
+        
+# improved version:
+from typing import List
+
+class Solution:
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        if len(s) < 10:
+            return []
+
+        seen, repeated = set(), set()
+        
+        for i in range(len(s) - 9):
+            window = s[i:i + 10]
+            if window in seen:
+                repeated.add(window)
+            else:
+                seen.add(window)
+                
+        return list(repeated)
