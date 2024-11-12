ceb0cd76b01c52a73903b203688af316b173576c
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2024-11-12 09:15:59 -0500

    Update Longest Repeating Character Replacement.py

--- Longest Repeating Character Replacement.py
+++ Longest Repeating Character Replacement.py
@@ -1,1 +1,7 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        # 1) find the identical string window
+        # 2) always replace it to get the max length
+        # 3) store the max length through iteration
 
+        
