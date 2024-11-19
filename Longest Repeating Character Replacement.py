21e8ddac33d97b2ec55cec3cd4fdfa0553f217aa
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2024-11-19 06:27:12 -0500

    Update Longest Repeating Character Replacement.py

--- Longest Repeating Character Replacement.py
+++ Longest Repeating Character Replacement.py
@@ -4,4 +4,35 @@ class Solution:
         # 2) always replace it to get the max length
         # 3) store the max length through iteration
 
-        
+        if k >= len(s):
+            return len(s)
+
+        # str_win = 
+        max_rep = k
+        ma_sub = k 
+
+        #use left, right to define the window
+        left, right = 0, 0
+        while left <= right and right <= len(s):
+
+            curr_char = s[left] # for each char, we need to find the longest possible substring
+            
+            while left <= right and right <= len(s) and k != 0:
+                print(s[left], s[right+1])
+                if s[left] != s[right+1]:
+                    print(s[left], s[right+1])
+                    exit()
+                exit()
+            # print(s[left:right])
+
+            # #update win:
+            # right += k
+            # print(s[left:right+1])
+
+            # if s[left] == s[right+1]:
+            #     print('same')
+            # print(s[left], s[right+1])
+
+            
+
+            exit()
