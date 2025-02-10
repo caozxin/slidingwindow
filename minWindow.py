a3f6ffc73ba8fe05feed6840237a288e63d215cf
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2025-02-10 10:53:56 -0500

    Update minWindow.py

--- minWindow.py
+++ minWindow.py
@@ -3,52 +3,33 @@ from collections import Counter
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         # return
-
-        def check_counters(counter_a: list, counter_b: list):
-
-            for each in counter_a:
-                if counter_a[each] <= counter_b[each]:
-                    # print("matching")
-                    pass
-                else:
-                    return False
-            return True
-
-
-        if  not s or not t: # none handling
+        # print("hello")
+        if not s or not t:
             return ""
-        # s = 'a'
-        # t = 'aa'
-        count_t = Counter(t)
+        # --> below are required
+        # t = 'a'
+        # s = 'abs'
+        count_t = Counter(t) 
+        len_t = len(t)
+        min_sub_b = ""
 
-        print(count_t)
+        print(count_t, len_t)
 
-        count_s = Counter(s)
-
-        if not check_counters(count_t,count_s): # if the whole string does not contain t at all
-            return ""
-
-        print(check_counters(count_t,count_s))
-
-        # left, right = 0, 0
-        min_sub = len(s)
-        min_sub_s = ''
-
-
+        left, right = 0, 0
         for left in range(len(s)):
-            right = left
-            sub_s = s[left:right+1]
-            print(sub_s)
-            while check_counters(Counter(sub_s), count_t) and right < len(s):
-                print("true sub", sub_s, left, right)
-                if min_sub > len(sub_s):
-                    print("sub_s", sub_s)
-                    min_sub_s = sub_s
-                    min_sub = len(min_sub_s)
-                right +=1
-                sub_s = s[left:right+1] 
-            print("not true sub")
-            exit()
-        
+            
+            sub_s = s[left:right + 1]
+            curr_len = right - left + 1
 
+            if right < len(s) and curr_len < len_t: #1) condition
+                right += 1
+            elif set(sub_s).issubset(set(t)): # 2) condition, atm curr_len >= len_t:
+                print(sub_s)
+                print("matching t")
+                if len(min_sub_b) == 0 or len(sub_s) <= len(min_sub_b):
+                    min_sub_b = sub_s
+                    print("new min: ", min_sub_b)
+                left += 1
+            # exit()
 
+        return min_sub_b
