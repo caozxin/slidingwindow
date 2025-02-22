da0fc55d037cbb7cd467ac0cfdd6c52250e658c3
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2025-02-22 15:14:28 -0500

    Update minSubArrayLen.py

--- minSubArrayLen.py
+++ minSubArrayLen.py
@@ -1,29 +1,21 @@
+from typing import List
+
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-
-        if not nums: # none handling
+        if not nums:
             return 0
         
-        # set variables
         n = len(nums)
         min_len = float('inf')
         curr_sum = 0
-        left, right = 0, 0
+        left = 0
 
         for right in range(n):
-            curr_len = right - left + 1
             curr_sum += nums[right]
-            window = nums[left:right+1]
-            print(curr_sum, window)
-
-            if curr_sum < target:
-                min_len = min(min_len, right - left + 1)
-                right += 1
-                
-            else:
-                left += 1
-                curr_sum -= nums[left]
-                # print("curr_len", curr_len, window)
-                # print("min_len", min_len)
             
-        return min_len if min_len < n else 0
+            while curr_sum >= target:  # Try to shrink the window
+                min_len = min(min_len, right - left + 1)
+                curr_sum -= nums[left]
+                left += 1  # Move the left boundary
+                
+        return min_len if min_len != float('inf') else 0
